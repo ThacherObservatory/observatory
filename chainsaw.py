4e37a03afd927e684a26db71a72eeacd7de50599
nedwardsthacher <nedwards@thacher.org>
2015-11-20 10:28:47 -0800

    Added initial scipt that will be used to correlate the seeing and weather data called "chainsaw.py"

---
+++ chainsaw.py
@@ -0,0 +1,10 @@
+"""
+I need some help with the path manager thing
+It's not recognizing weather and seeing
+"""
+
+import weather as w
+import seeing as s
+
+weather_data = w.get_data()
+seeing_data = s.get_data()
