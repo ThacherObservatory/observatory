f22238813c6af93748604a4414dd91d57f1f7e3c
jswift <jswift@thacher.org>
2015-11-21 21:40:34 -0800

    Read in overlapping seeing and weather data. Grid the data to the same timestamp. Next step is to plot the 2D correlation of the data. See comments in the script.

--- chainsaw.py
+++ chainsaw.py
@@ -17,14 +17,17 @@ import matplotlib.pyplot as plt
 from statsmodels.nonparametric.kernel_density import KDEMultivariate as KDE
 import robust as rb
 import matplotlib as mpl
+from scipy.interpolate import interp1d
+import calendar
 
 
 wpath = '/Users/nickedwards/python/data/weather/'
 spath = '/Users/nickedwards/python/data/seeing/'
-weather_data = w.get_data(dpath=wpath)
-seeing_data = s.get_data(path=spath)
+# Get raw data. Must choose an overlapping time range
+weather_data = w.get_data(dpath=wpath,year=2015)
+seeing_data = s.get_data(path=spath,year=2015,month=3,day=6)
 
-#These are plot limits
+# Plot limits, will update later
 
 #min1 = 0.0
 #max1 = 360.0
@@ -32,11 +35,53 @@ seeing_data = s.get_data(path=spath)
 #sigfac = 3
 #sigsamp = 5
 
+# Get data of interest
 
-dist1 = seeing_data['FWHMave']
-dist2 = weather_data['humidity']
-year = weather_data['year']
-fs = 20
+# FWHM data must be vetted for outliers first
+dt_seeing,fwhm = s.vet_FWHM_series(seeing_data['datetime'],seeing_data['FWHMave'])
+
+# Weather data
+humidity   = weather_data['humidity']
+dt_weather = weather_data['datetime']
+
+# Make sure data overlap in time...
+# select out the indices of the weather data that overlap
+# with the seeing data
+inds, = np.where((dt_weather >= np.min(dt_seeing)) &
+                 (dt_weather <= np.max(dt_seeing)))
+dt_weather = dt_weather[inds]
+humidity = humidity[inds]
+
+inds, = np.where((dt_seeing >= np.min(dt_weather)) &
+                 (dt_seeing <= np.max(dt_weather)))
+dt_seeing = dt_seeing[inds]
+fwhm = fwhm[inds]
+
+# Take a look
+plt.ion()
+plt.figure(1)
+plt.clf()
+plt.plot(dt_seeing,fwhm,'o',label='FWHM')
+plt.plot(dt_weather,humidity,'o',label='humidity')
+plt.xlim(np.min(dt_seeing),np.max(dt_seeing))
+
+# Grid weather data to seeing data timestamp
+
+# need to turn datetime objects into numerical dates, first
+def toTimestamp(d):
+  return calendar.timegm(d.timetuple())
+
+tseeing  = np.array([toTimestamp(d) for d in dt_seeing])
+tweather = np.array([toTimestamp(d) for d in dt_weather])
+
+# now interpolate
+interp_func = interp1d(tweather,humidity,kind='linear')
+humidity_interp = interp_func(tseeing)
+
+plt.plot(dt_seeing,humidity_interp,'-',label='interpolated humidity')
+plt.legend(loc='best')
+
+
 
 med1 = np.median(dist1)
 sig1 = rb.std(dist1)
