0c9aad0889954ddedea5dd772ad3b9ec939abbed
nedwardsthacher <nedwards@thacher.org>
2015-11-21 19:57:12 -0800

    Changed dist variables, added fs and year variable. To do:Match weather data to seeing data by time. Start to make graphs and fix plot limits

--- chainsaw.py
+++ chainsaw.py
@@ -1,12 +1,22 @@
 """
-I need some help with the path manager thing
-It's not recognizing weather and seeing
+To Do:
+Match weather data to seeing data by time. 
+Start to make graphs and fix plot limits
+
+History:
+
+11/21/15
+nedwards: Changed dist variables, added fs and year variable
+
 """
 
 import weather as w
 import seeing as s
 import numpy as np
 import matplotlib.pyplot as plt
+from statsmodels.nonparametric.kernel_density import KDEMultivariate as KDE
+import robust as rb
+import matplotlib as mpl
 
 
 wpath = '/Users/nickedwards/python/data/weather/'
@@ -14,38 +24,20 @@ spath = '/Users/nickedwards/python/data/seeing/'
 weather_data = w.get_data(dpath=wpath)
 seeing_data = s.get_data(path=spath)
 
+#These are plot limits
 
-# Following code was Frankensteined from weather.py to be used as
-# as template for a 2D correlation plot
-from statsmodels.nonparametric.kernel_density import KDEMultivariate as KDE
-import robust as rb
+#min1 = 0.0
+#max1 = 360.0
+#min2 = 0
+#sigfac = 3
+#sigsamp = 5
 
-min1 = 0.0
-max1 = 360.0
-min2 = 0
-sigfac = 3
-sigsamp = 5
 
-d = get_data(year=year)
-if peak:
-    wind = d['windhi']
-    tag = 'peak'
-    word = 'Peak '
-else:
-    wind = d["wind"]
-    tag = 'ave'
-    word = 'Average '
-        
-wdir = d["winddir_deg"]
+dist1 = seeing_data['FWHMave']
+dist2 = weather_data['humidity']
+year = weather_data['year']
+fs = 20
 
-wind_rand = wind + np.random.normal(0,0.5,len(wind))
-wdir_rand = wdir + np.random.normal(0,12,len(wdir))
-bad = np.isnan(wdir_rand)
-wdir_rand[bad] = np.random.uniform(0,360,np.sum(bad))
-
-dist1 = wdir_rand
-dist2 = wind_rand
-    
 med1 = np.median(dist1)
 sig1 = rb.std(dist1)
 datamin1 = np.min(dist1)
@@ -67,7 +59,7 @@ Z = np.reshape(kernel.pdf(positions).T, X.shape)
 
 aspect = (max1-min1)/(max2-min2) * 8.5/11.0
 
-plot_params()
+w.plot_params()
 plt.ion()
 plt.figure(2,figsize=(11,8.5))
 plt.clf()
@@ -75,9 +67,9 @@ ax = plt.subplot(111)
 ax.imshow(np.rot90(Z), cmap=plt.cm.CMRmap_r,aspect=aspect, \
           extent=[min1, max1, min2, max2],origin='upper')
 ax.yaxis.labelpad = 12
-ax.set_xlabel('Wind Direction (degrees)',fontsize=fs)
-ax.set_ylabel(word+'Wind Speed (mph)',fontsize=fs)
-plt.title('Wind Patterns at Thacher Observatory in '+str(year),fontsize=fs)
+ax.set_xlabel(str(dist1),fontsize=fs)
+ax.set_ylabel(str(dist2),fontsize=fs)
+plt.title('Weather and Seeing Corralation at Thacher Observatory in '+str(year),fontsize=fs)
 
-plt.savefig('Wind'+tag+'_Speed_Direction_'+str(year)+'.png',dpi=300)
+plt.savefig(str(dist1)+'by'+str(dist2)+str(year)+'.png',dpi=300)
 mpl.rcdefaults()
