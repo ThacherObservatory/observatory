c247bdc39460e2929c6009e8b7768a9657dd3751
jswift <jswift@thacher.org>
2016-03-22 20:38:22 -0700

    Minor changes to thacher_obs.

--- thacher_obs.py
+++ thacher_obs.py
@@ -3,7 +3,7 @@ thob = ephem.Observer()
 thob.long = ephem.degrees("-119.1773417")
 thob.lat = ephem.degrees("34.467028")
 thob.elevation = 504.4 
-thob.date = "2017/8/21" 
+thob.date = "2017/8/21 13.0" 
 sun = ephem.Sun(thob)
 print sun.alt, sun.az
 print sun.ra, sun.dec
