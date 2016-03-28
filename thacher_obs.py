fce841fc7fd03551ec75174fa4587d1f5588ece9
Sara Yin <syin@thacher.org>
2016-03-27 21:32:28 -0700

    Update thacher_obs.py

--- thacher_obs.py
+++ thacher_obs.py
@@ -18,14 +18,31 @@ def timeloop(startDate, endDate, delta=timedelta(days=1)):
 x = []
 y = []
 
+m = []
+n = []
+
 for t in timeloop(datetime(2016, 3, 29, 01, 00, 00), 
                           datetime(2016, 3, 30, 01, 00, 00), 
                           delta=timedelta(hours=1)):
     thob.date = t
     sun = ephem.Sun(thob)
-    x.append(sun.alt) 
-    y.append(sun.az)
+    x.append(sun.az) 
+    y.append(sun.alt)
+    m.append(sun.ra) 
+    n.append(sun.dec)
+    print"%s %f %f %f %f" % (thob.date, sun.alt, sun.az, sun.ra, sun.dec)
+    
+horizontal_coordinates = pl.figure(1)
+pl.subplot(212)
+pl.title('Path of our SUN in one day')
+pl.xlabel('Azimuth')
+pl.ylabel('Altitude')
+pl.plot(m,n,'yo')
+pl.show()
 
+Equatorial_coordinates = pl.figure(2)
+pl.title('Path of our SUN in one day')
+pl.xlabel('Right Ascension')
+pl.ylabel('Declination')
 pl.plot(x,y,'ro')
 pl.show()
-print"%s %f %f %f %f" % (thob.date, sun.alt, sun.az, sun.ra, sun.dec)
