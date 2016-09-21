a308adc498f587e21952c7f8afcbd98463f7c57d
nedwardsthacher <nedwards@thacher.org>
2016-09-20 22:06:05 -0700

    Oiled and refuelled

--- chainsaw.py
+++ chainsaw.py
@@ -1,11 +1,10 @@
 ##########################################
 #
 #    To Do (WEDNESDAY):
-# Check for weirdness in OutHum
 #
 #
 #   MEETING (MONDAY):
-# Comparing datetime objects
+#
 #
 #
 ##########################################
@@ -18,20 +17,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from statsmodels.nonparametric.kernel_density import KDEMultivariate as KDE
 import robust as rb
-import matplotlib as mpl
 from scipy.interpolate import interp1d
 import calendar,sys
 import pdb
 from scipy.stats.stats import pearsonr
-from matplotlib.ticker import NullFormatter
 import cloudsensor as cs
 import datetime
 import glob
 from plot_params import *
+import pandas as pd
 
+
+#for quick display of seeing
+#fix x axis
 def seeing_hist():
     plot_params()
-    FWHM, sdate = load_data_seeing(year=[2015,2016],month=[10,4],day=[6,17])
+    FWHM, sdate = load_sdata(year=[2015,2016],month=[3,6],day=[6,14])
     plt.ion()
     plt.clf()
     plt.figure(1)
@@ -43,16 +44,20 @@ def seeing_hist():
     plt.annotate('Median = %.2f' % med, [.85,.8], horizontalalignment='right',xycoords='figure fraction',fontsize=15)
     return FWHM, sdate
 
-#Make this more robust
-def interpolate(plot_interp=False,plot_corr=False,weather_key='OutHum'):
 
-    dt_seeing = sdatetime
-    seeing = FWHMave
+#Make this more robust
+def interpolate(plot_interp=False,plot_corr=False,weather_key='OutHum',dt_seeing=sdatetime,seeing=FWHMave):
+
     # Weather data
-    weather = weather_data[weather_key]
+    weather = wdata[weather_key]
 
     #changes weather time data to datetime to work with seeing time data
-    dt_weather = np.array([t.to_datetime() for t in weather_data['datetime']])
+    ts_weather = np.array([pd.Timestamp(ts) for ts in vwdatetime])
+    dt_weather = np.array([t.to_datetime() for t in ts_weather])
+
+    #if statment to
+    if np.where(weather == '---'):
+        dt_weather, weather = vet_wdata(dt_weather,weather)
 
     # Make sure data overlap in time...
     # select out the indices of the weather data that overlap
@@ -68,18 +73,21 @@ def interpolate(plot_interp=False,plot_corr=False,weather_key='OutHum'):
     dt_seeing = dt_seeing[inds]
     seeing = seeing[inds]
 
+    #changing to TimeStamp so it's readable
     def toTimestamp(d):
         return calendar.timegm(d.timetuple())
 
     tseeing  = np.array([toTimestamp(d) for d in dt_seeing])
     tweather = np.array([toTimestamp(d) for d in dt_weather])
 
+    #actual interpolation
     interp_func = interp1d(tweather,weather,kind='linear')
     weather_interp = interp_func(tseeing)
 
+    #plot interpoalted seeing and weather
     if plot_interp:
         plt.ion()
-        plt.figure(1)
+        plt.figure(10000000000000000000000)
         plt.clf()
         plt.plot(dt_seeing,seeing,'o',label='FWHM')
         plt.plot(dt_weather,weather,'o',label=weather_key)
@@ -87,25 +95,34 @@ def interpolate(plot_interp=False,plot_corr=False,weather_key='OutHum'):
         plt.plot(dt_seeing,weather_interp,'-',label='interpolated humidity')
         plt.legend(loc='best')
 
-    #FINISH THIS MAKING SEPERATE PLOT FOR SEEING VS WEATHER_INTERP
+    #correlation plot with pearson r coeffiecent
     if plot_corr:
         plt.ion()
-        plt.figure(2)
+        plt.figure(weather_key)
+        r = pearsonr(seeing, weather_interp)
         plt.clf()
-        plt.plot(seeing,weather_interp,'o')
+        plt.plot(seeing,weather_interp,'.')
         plt.xlabel('FWHM (arcsec)')
-        plt.ylabel('Humidity (%)')
-    return pearsonr(seeing, weather_interp)
+        plt.ylabel(weather_key)
+        plt.annotate('pearson r = ' + str(r),[.85,.8], horizontalalignment='right',xycoords='figure fraction',fontsize=15)
+
+    #calculating and returning pearson r coeffiecent
+    r = pearsonr(seeing, weather_interp)
+    return r
 
 #cloudsensor data function
-# 5/11/15 4:20 - 4:35 p
-# np.where(weather_data['OutHum'] == '---')
-def load_data_seeing(year=[2016],month=[3],day=[20]):
+def load_sdata(year=[2016],month=[3],day=[20]):
+
+    #naming path for data
     spath = '/Users/nickedwards/Dropbox (Thacher)/Observatory/Seeing/Data/'
+
+    #turning all useful arrays into numpy arrays
     year = np.array(year)
     month = np.array(month)
     day = np.array(day)
 
+    #in order to get a range of days for fwhm values start and stop dates are
+    #created to cycle through the dates
     if len(year)==1:
         start_yr = year
         stop_yr = year
@@ -118,10 +135,13 @@ def load_data_seeing(year=[2016],month=[3],day=[20]):
         start_dy = day
         stop_dy = day
 
+    #not allowing arrays larger than 2 because that doesn't make sense
     if len(year) > 2 or len(month) > 2 or len(day) > 2:
         print "Fix year, month, or day amount"
         return []
 
+    #if the arrays are are 2 indicies long beginning index is for start date
+    #and second index is for stop date
     if len(year)==2:
         start_yr = year[0]
         stop_yr = year[1]
@@ -135,6 +155,7 @@ def load_data_seeing(year=[2016],month=[3],day=[20]):
         start_dy = day[0]
         stop_dy = day[1]
 
+    #actual creation of datetime objects
     start_dt = datetime.datetime(start_yr,start_mn,start_dy)
     stop_dt = datetime.datetime(stop_yr,stop_mn,stop_dy)
 
@@ -171,22 +192,22 @@ def load_data_seeing(year=[2016],month=[3],day=[20]):
         t = gooddates[i] #(.year .month. day)
         sdata = s.get_data(path=spath,year=t.year,month=t.month,day=t.day)
         FWHM_series = s.FWHM_ave(sdata)
-        FWHM_vet = s.vet_FWHM_series(sdata['datetime'],FWHM_series)
+        newtime, FWHM_vet = s.vet_FWHM_series(sdata['datetime'],FWHM_series)
         FWHMave = np.append(FWHMave,FWHM_vet)
-        sdatetime = np.append(sdatetime,sdata['datetime'])
-        pdb.set_trace()
+        sdatetime = np.append(sdatetime,newtime)
+
     #FWHMave = s.FWHM_ave(sdata)
     #sdatetime,FWHMave = s.vet_FWHM_series(sdatetime,FWHMave)
 
     return FWHMave, sdatetime
 
-def load_data_weather(year=[2016]):
+def load_wdata(year=[2016]):
     wpath = '/Users/nickedwards/Dropbox (Thacher)/Observatory/Weather/Data/'
 
     if len(year)==1:
         year = year[0]
-        weather_data = w.get_data(dpath=wpath,year=year)
-        return weather_data
+        wdata = w.get_data(dpath=wpath,year=year)
+        return wdata
 
     elif len(year)==2:
         start_yr = year[0]
@@ -210,48 +231,16 @@ def load_data_weather(year=[2016]):
         start_data = w.get_data(dpath=wpath,year=start_yr)
         stop_data = w.get_data(dpath=wpath,year=stop_yr)
         # fix dtypes
-        weather_data = {}
+        wdata = {}
         for key in start_data.keys():
-            weather_data[key] = np.append(start_data[key],stop_data[key])
-        return weather_data
+            wdata[key] = np.append(start_data[key],stop_data[key])
+        return wdata
 
     else:
         print "Fix year amount."
         return []
 
-def vet(data,keyword=False):
-    # Function that vets for keyword(s)
-    if keyword:
-        if len(keyword) == 1:
-            vetted_data = np.delete(np.array(data), np.where(np.array(data) == keyword[0]))
-            vets = np.array(data)[np.where(np.array(data) == keyword[0])]
-            return vetted_data, vets
-
-
-
-        """
-        vetted_data = []
-        vets = []
-        for i in range(len(keyword)):
-            # loop doesn't work because
-            ""
-            data = ['---','nan',123,12333]
-
-            keyword = ['nan','---']
-
-            if keyword:
-                vetted_data = []
-                vets = []
-                for i in range(len(keyword)):
-                    vetted_data = np.append(vetted_data,np.delete(np.array(data), np.where(np.array(data) == keyword[i])))
-                    vets = np.append(vets,np.array(data)[np.where(np.array(data) == keyword[i])])
-
-
-            vets == array(['nan', '---'], dtype='|S32')
-
-            vetted_data == array(['---', '123', '12333', 'nan', '123', '12333'], dtype='|S32')
-            ""
-            vetted_data = np.append(vetted_data,np.delete(np.array(data), np.where(np.array(data) == keyword[i])))
-            vets = np.append(vets,np.array(data)[np.where(np.array(data) == keyword[i])])
-            return vetted_data, vets
-            """
+def vet_wdata(datetime,data,keyword='---'):
+    vetted_data = np.delete(np.array(data), np.where(np.array(data == '---')))
+    vetted_datetime = np.delete(np.array(datetime), np.where(np.array(data == '---')))
+    return vetted_datetime, vetted_data
