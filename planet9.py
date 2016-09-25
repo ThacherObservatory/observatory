f20a73b1f2b37ca5f4c65c0fc2babea22faa945e
nedwardsthacher <nedwards@thacher.org>
2016-09-25 13:59:33 -0700

    script for all you planet9 needs

---
+++ planet9.py
@@ -0,0 +1,51 @@
+# Nick Edwards
+# 09/21/2016
+# This code is for determining the various properties of the SNR equation
+
+import numpy as np
+import SNR
+import matplotlib.pyplot as plt
+
+
+# Make sure vatiables are floats to avoid int division
+# snr should be between 5 - 20
+# mlim should be between 22.5 - 25
+
+# Standard variables
+npix = 25.9
+Fb = 3.0
+g = 1.9
+mzp = 22.5
+
+def integrationTime(snr,mlim):
+    snr = np.array(snr)
+    mlim = np.array(mlim)
+    t = npix*Fb/np.power((g/snr)*np.power(10.0,-.4*(mlim-mzp)),2)
+    return t
+
+def contourTime(s=[5.0,10.0],m=[22.5,25.0]):
+    s = np.linspace(s[0],s[1],1000)
+    m = np.linspace(m[0],m[1],1000)
+    snr, mlim = np.meshgrid(s,m)
+    time = integrationTime(snr,mlim)
+    timeMins = time/60
+    plt.ion()
+    plt.figure()
+    plt.clf()
+    plot = plt.contour(snr,mlim,timeMins,200,cmap='inferno')
+    plt.clabel(plot, inline=True, fontsize=10)
+    plt.colorbar(plot,label='Time (mins)')
+    plt.xlabel('SNR')
+    plt.ylabel('Mlim')
+    plt.title('Integration time as a\nfunction of SNR and Mlim')
+
+def p9Region():
+    RA = np.loadtxt("/Users/nickedwards/Downloads/P9BlackOnly.txt")[:,0]
+    Dec = np.loadtxt("/Users/nickedwards/Downloads/P9BlackOnly.txt")[:,1]
+    plt.ion()
+    plt.figure()
+    plt.clf()
+    plt.plot(RA,Dec,'.')
+    plt.xlabel('RA')
+    plt.ylabel('Dec')
+    plt.title('Region, in RA and Dec\nwhere Planet 9 could be')
