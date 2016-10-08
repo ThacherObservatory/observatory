864f998388ead68c8fc9a718d577e8bcff5c6a70
glaw1300 <glawrence@thacher.org>
2016-10-07 23:50:01 -0700

    1D gaussian finished. Selected section now a circle.

--- fitgaussian.py
+++ fitgaussian.py
@@ -8,7 +8,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from astropy.io import fits
 import glob
-import matplotlib.mlab as mlab
 from scipy.stats import norm
 
 def makeGaussian(deg=20.,arcppx=383.65, center=None, dir="/Users/george/Dropbox/Astronomy/AllSky Cam/30Sept2016/", filename="sky1.FIT"):
@@ -37,21 +36,18 @@ def makeGaussian(deg=20.,arcppx=383.65, center=None, dir="/Users/george/Dropbox/
     else:
         ycent = yd//2
         xcent = xd//2
-    # domain and range of values you want selected out of table for gaussian
-    xmin = xcent-fwhm//2
-    xmax = xcent+fwhm//2
-    ymin = ycent-fwhm//2
-    ymax = ycent+fwhm//2
+    #Make circle range to extract circular section of datapoints from main img (rad = fwhm/2 b/c fwhm = width of portion, rad = 1/2width)
+    rad = fwhm/2
+        #np.ogrid returns np array of yvalues going down indexed at [0] ([0][1][2]...), xvalues grid indexed at [1] ([0,1,2,3...])
+    y,x = np.ogrid[ :yd, :xd]
+    circ = (x-xcent)**2 + (y-ycent)**2 <= rad*rad
     # Add all datapoints that you want from image to gaussian zeros array, run thru a gaussian function to make data set of gaussian values
-    mean = np.mean(img[ymin:ymax,xmin:xmax])
-    imgaus[ymin:ymax,xmin:xmax] = (1.0/(np.sqrt(2.0*np.pi)*sig))*(np.e**((-1*(img[ymin:ymax,xmin:xmax]-mean)**2)/(2*sig**2)))
+    mean = np.mean(img[circ])
+    imgaus[circ] = (1.0/(np.sqrt(2.0*np.pi)*sig))*(np.e**((-1*(img[circ]-mean)**2)/(2*sig**2)))
     # plot gaussian distribution of points from original image (norm.pdf normalizes points for you, sig calculated earlier relative to fwhm)
-    mean = np.mean(img[ymin:ymax,xmin:xmax])
+    mean = np.mean(img[circ])
     plt.clf()
     plt.figure(1)
-    plt.plot(img[ymin:ymax,xmin:xmax],norm.pdf(img[ymin:ymax,xmin:xmax],mean,sig))
+    plt.plot(img[circ],norm.pdf(img[circ],mean,sig))
     plt.show()
-    """
-    comb outliers?
-    """
-    return imgaus[ymin:ymax, xmin:xmax]
+    return imgaus[circ]
