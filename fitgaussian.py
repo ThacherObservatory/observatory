fee8c49d63d6e82241c7dd0a9d6583d0abd90a83
syin@thacher.org <syin@thacher.org>
2016-10-10 20:21:55 -0700

    3d graph: it's upside down, also, my computer crashed 5 times while running it, so beware

--- fitgaussian.py
+++ fitgaussian.py
@@ -9,8 +9,9 @@ import numpy as np
 from astropy.io import fits
 import glob
 from scipy.stats import norm
+from mpl_toolkits.mplot3d import Axes3D
 
-def makeGaussian(deg=20.,arcppx=383.65, center=None, dir="/Users/george/Dropbox/Astronomy/AllSky Cam/30Sept2016/", filename="sky1.FIT"):
+def makeGaussian(fwhm=20.,arcppx=383.65, center=None, dir="/Users/sara/python/30Sept2016/", filename="sky1.FIT"):
     """
     fwhm: full width half max
     arcppx: sqarcsec per pixel
@@ -21,7 +22,7 @@ def makeGaussian(deg=20.,arcppx=383.65, center=None, dir="/Users/george/Dropbox/
     Takes image, makes a 2D gaussian of selected portion of sky
     """
     #Calculates sigma and the number of pixels wide the circle will be
-    fwhm = (deg*3600)/arcppx
+    fwhm *= 3600/arcppx
     sig = fwhm/(2*np.sqrt(2*np.log(2)))
     #Creates two arrays: array of image and array of zeros w/ same dimensions
     hdu = fits.open(dir+filename)[0]
@@ -29,13 +30,15 @@ def makeGaussian(deg=20.,arcppx=383.65, center=None, dir="/Users/george/Dropbox/
     yd = hdu.header['NAXIS2']
     img = fits.getdata(dir+filename)
     imgaus = np.zeros((yd, xd))
-    #Creates variables for x and y centers
+    #Creates variables for x and y
+
     if center:
         ycent = center[1]
         xcent = center[0]
     else:
         ycent = yd//2
         xcent = xd//2
+            
     #Make circle range to extract circular section of datapoints from main img (rad = fwhm/2 b/c fwhm = width of portion, rad = 1/2width)
     rad = fwhm/2
         #np.ogrid returns np array of yvalues going down indexed at [0] ([0][1][2]...), xvalues grid indexed at [1] ([0,1,2,3...])
@@ -44,10 +47,41 @@ def makeGaussian(deg=20.,arcppx=383.65, center=None, dir="/Users/george/Dropbox/
     # Add all datapoints that you want from image to gaussian zeros array, run thru a gaussian function to make data set of gaussian values
     mean = np.mean(img[circ])
     imgaus[circ] = (1.0/(np.sqrt(2.0*np.pi)*sig))*(np.e**((-1*(img[circ]-mean)**2)/(2*sig**2)))
-    # plot gaussian distribution of points from original image (norm.pdf normalizes points for you, sig calculated earlier relative to fwhm)
-    mean = np.mean(img[circ])
+    # plot gaussian distribution of points from original image (norm.pdf normalizes points for you, sig calculated earlier relative to fwhm)    
+    arrayx = np.argwhere(circ!=False)
+    arrayy = []
+    arrayz = []
+    cy = []
+    cz = []
+    
+    for j,ys in enumerate(arrayx):
+        cy.append(ys[0])
+        cy.append(imgaus[circ][j])
+        arrayy.append(cy)
+        cz.append(ys[1])
+        cz.append(imgaus[circ][j])
+        arrayz.append(cz)
+        
     plt.clf()
-    plt.figure(1)
-    plt.plot(img[circ],norm.pdf(img[circ],mean,sig))
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(arrayx, arrayy, arrayz) #img[circ],norm.pdf(img[circ],mean,sig))
     plt.show()
-    return imgaus[circ]
+    
+    return arrayx
+
+  """  
+    for i,row in enumerate(circ):
+        for j, pixel in enumerate(row):
+            if pixel != False:
+                cx.append(i)
+                cx.append(j)
+                arrayx.append(cx)
+                cy.append(i)
+                index = imgaus[circ].index([i,j])
+                cy.append(imgaus[circ][index])
+                arrayy.append(cy)
+                cz.append(j)
+                cz.append(imgaus[circ][index])
+                arrayz.append(cz)
+    """
