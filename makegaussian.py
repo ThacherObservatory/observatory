6fd29279b189ca7226636f496511da5af3372b58
glaw1300 <glawrence@thacher.org>
2016-10-26 13:48:29 -0700

    returns weighted mean in magnitudes comparing to photometer

--- makegaussian.py
+++ makegaussian.py
@@ -11,8 +11,9 @@ import glob
 from scipy.stats import norm
 from mpl_toolkits.mplot3d import Axes3D
 
-def makeGaussian(fwhm=20.,arcppx=383.65, center=None, dir="/Users/sara/python/30Sept2016/", filename="sky1.FIT"):
+def makeGaussian(m0, fwhm=20.,arcppx=383.65, center=None, dir="/Users/sara/python/30Sept2016/", filename="sky1.FIT"):
     """
+    m0: reading from photometer
     fwhm: full width half max
     arcppx: sqarcsec per pixel
     center: if gaussian to be centered around certain points, say so in tuple/list
@@ -28,46 +29,31 @@ def makeGaussian(fwhm=20.,arcppx=383.65, center=None, dir="/Users/sara/python/30
     hdu = fits.open(dir+filename)[0]
     xd = hdu.header['NAXIS1']
     yd = hdu.header['NAXIS2']
-    #img = fits.getdata(dir+filename)
-    #imgaus = np.zeros((yd, xd))
     #Creates variables for x and y
-
     x = np.arange(0, xd, 1, float)
     y = np.arange(0, yd, 1, float)
     y = y[:,np.newaxis]
-
+    # Asssume center of brightness is at center of image unless specified otherwise
     if center:
         y0 = center[1]
         x0 = center[0]
     else:
         y0 = yd//2
         x0 = xd//2
-    #[X,Y] = np.meshgrid(x,y)
-    #Z = (1.0/(np.sqrt(2.0*np.pi)*sig))*(np.exp(-((X-x0)**2+(Y-y0)**2)/(2.0*sig**2))
+    # Gaussian
     Z = (1.0/(np.sqrt(2.0*np.pi)*sig))*(np.exp(-4*np.log(2) * ((x-x0)**2 + (y-y0)**2) / (2.0*sig**2)))
+    # image
+    N = fits.getdata(dir+filename)
+    # Weighted Mean
+    wmean = np.sum(N*Z)/np.sum(Z)
+    # Image in magnitudes
+    img_mag = m0 - 2.5*np.log(N/wmean)
+    # Returns weighted mean of the image in magnitudes
+    return np.sum(img_mag*Z)/np.sum(Z)
+"""
+m-m0 = -2.5log(F/F0)
+m0 = 20.78
+F0 = wmean
 
-    imgZ = 2.5*np.log(Z)
-
-    plt.ion()
-    plt.clf()
-    plt.figure(99,figsize=(15,8))
-    plt.imshow(Z,cmap='pink',interpolation='nearest',origin='upper')
-    #fig = plt.figure()
-    #ax=fig.add_subplot(111,projection='3d')
-    #ax.plot_surface(x,y,Z,)
-    plt.colorbar()
-    #plt.show()
-
-    #def weightedMean(dir="/Users/sara/python/30Sept2016/", filename="sky1.FIT"):
-    N = 2.5*np.log(fits.getdata(dir+filename)[0])
-    W = imgZ
-
-    #mean = np.sum(N)/np.float(len(N))
-    wmean = np.sum(N*W)/np.sum(W)
-
-    #plt.figure(99,figsize=(15,8))
-    #plt.imshow(wmean,cmap='pink',interpolation='nearest',origin='upper')
-    #plt.colorbar()
-    #plt.show()
-
-    return 20.78/wmean
+img_magnitude = m0-2.5log10(img/wmean)
+"""
